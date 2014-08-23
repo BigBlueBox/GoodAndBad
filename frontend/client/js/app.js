312d47931e85d5513c3ac433fd8ad641f2a59e0e
Kegan Myers <kegan@keganmyers.com>
2014-08-23 11:19:56 -0500

    Add d3, jquery, and bootstrap to browserify

--- frontend/client/js/app.js
+++ frontend/client/js/app.js
@@ -1,2 +1,4 @@
-var $, d3 = require('d3');
-var jQuery = $ = require('jquery');
+var $, jQuery, d3 = require('d3');
+//noinspection JSValidateTypes
+window.$ = window.jQuery = $ = jQuery = require('jquery/dist/jquery.js');
+require('bootstrap/dist/js/bootstrap.js');

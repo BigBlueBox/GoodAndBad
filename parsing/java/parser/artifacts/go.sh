e3fa025b422b076e223e030567161550ee4aed4c
Jenny Brown <jenny@42lines.net>
2014-08-24 03:10:56 -0500

    recreated go.sh after file size problem with stanford corenlp

---
+++ parsing/java/parser/artifacts/go.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -cp lib/ejml-0.23.jar:lib/javax.json-api-1.0.jar:lib/jaxb-api-2.2.7.jar:lib/joda-time-2.1.jar:lib/jollyday-0.4.7.jar:lib/junit-3.8.1.jar:lib/mongo-java-driver-2.12.3.jar:lib/stanford-corenlp-3.4.jar:lib/stanford-corenlp-3.4-models.jar:lib/xalan-2.7.0.jar:lib/xercesImpl-2.8.0.jar:lib/xml-apis-1.3.03.jar:lib/xom-1.2.10.jar:parser.jar -jar parser.jar 65.254.105.151 27001 dataset1 c:\\globalhack 40
+

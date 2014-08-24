caaf9445795b5b2973e625dda5cf8fa60f4cd9bc
Jenny Brown <jenny@42lines.net>
2014-08-23 23:49:11 -0500

    Missed a file

---
+++ parsing/java/parser/src/main/java/com/bigbluebox/parser/NounVerbObject.java
@@ -0,0 +1,54 @@
+package com.bigbluebox.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NounVerbObject {
+    List<String> nouns = new ArrayList<String>();
+    List<String> verbs = new ArrayList<String>();
+    List<String> objects = new ArrayList<String>();
+
+    public NounVerbObject() {
+    }
+
+    public void addPOS(String word, String pos) {
+	if (pos.charAt(0) == 'N' && verbs.size() > 0) {
+	    objects.add(word);
+	} else if (pos.charAt(0) == 'N') {
+	    nouns.add(word);
+	}
+	if (pos.charAt(0) == 'V') {
+	    verbs.add(word);
+	}
+    }
+
+    public List<String> getNouns() {
+	return nouns;
+    }
+
+    public List<String> getVerbs() {
+	return verbs;
+    }
+
+    public List<String> getObjects() {
+	return objects;
+    }
+
+    public String toString() {
+	StringBuffer nvo = new StringBuffer();
+	for (String s : nouns) {
+	    nvo.append(s);
+	    nvo.append(" ");
+	}
+	for (String s : verbs) {
+	    nvo.append(s);
+	    nvo.append(" ");
+	}
+	for (String s : objects) {
+	    nvo.append(s);
+	    nvo.append(" ");
+	}
+	return nvo.toString().trim();
+    }
+
+}
